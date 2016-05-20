96934174de2ef99956005c966a6e7e2dafa6d8b2
Chovancová <Olga.Chovancova@st.fri.uniza.sk>
2016-05-20 12:27:58 +0200

    datepicker

--- addLogEntry.php
+++ addLogEntry.php
@@ -1,3 +1,22 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>jQuery UI Datepicker - Format date</title>
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
+    <script src="//code.jquery.com/jquery-1.10.2.js"></script>
+    <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+    <link rel="stylesheet" href="/resources/demos/style.css">
+    <script>
+        $(function() {
+            $( "#datepicker" ).datepicker();
+            $( "#datepicker" ).datepicker( "option", "dateFormat", "yy-mm-dd" );
+        });
+    </script>
+</head>
+<body>
+
+
 <?php
 include 'connect.php';
 
@@ -5,21 +24,22 @@ if ($_SERVER['REQUEST_METHOD'] != 'POST') {
     echo '<form class="form-addLogEntry" role="form" method="post" action="">
         <h2 class="form-addLogEntry-heading">Add Log Entry</h2>
         </br>
+        
          <label for="inputDate" class="sr-only">Date and Time - when the activity was performed</label>
-             <input type="date" name="log_date" id="log_date" class="form-control" placeholder="Log Date" required autofocus>
+            </br>  <input type="text" name="datepicker" id="datepicker" size="20" class="form-control" placeholder="Log Date" required autofocus>
         </br>
          <label for="inputDate" class="sr-only">How long lasts a particular activity - the number of hours and minutes</label>
-             <input type="time" name="log_time" id="log_time" class="form-control" placeholder="Log Time" required autofocus>
+          </br>    <input type="time" name="log_time" id="log_time" class="form-control" placeholder="Log Time" required autofocus>
           </br>      
          <label for="inputText" class="sr-only">A brief description of the activity</label>
-             <input type="textarea" name="log_description" id="log_time" class="form-control" placeholder="Log Description" required autofocus>
+          </br>    <input type="textarea" name="log_description" id="log_time" class="form-control" placeholder="Log Description" required autofocus>
      </br>
         <button class="btn btn-lg btn-primary btn-block" type="submit">Add activity</button>
       </form>';
 } else {
 
     $errors = array();
-    if (!isset($_POST['log_date'])) {
+    if (!isset($_POST['datepicker'])) {
         $errors[] = 'Log Date is required.';
     }
 
@@ -44,7 +64,7 @@ if ($_SERVER['REQUEST_METHOD'] != 'POST') {
         $sql = "INSERT INTO
                     log_entry(log_date, log_time, log_description, log_id_user)
                 VALUES(
-                    '" . mysql_real_escape_string($_POST['log_date']) . "',
+                    '" . mysql_real_escape_string($_POST['datepicker']) . "',
                     '" . mysql_real_escape_string($_POST['log_time']) . "',
                     '" . mysql_real_escape_string($_POST['log_description']) . "',
                     '" . mysql_real_escape_string($_SESSION['user_id']) . "'
